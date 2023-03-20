9762ff1419fd9d3da78e02c601ad77d03ba2f287
Mehdes <44497646+Mehdes@users.noreply.github.com>
2023-03-20 16:27:11 +0100

    Update streamlit_app.py
    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruits_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index))
-streamlit.dataframe(my_fruits_list)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
